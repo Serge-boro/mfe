75eb436c6cdabe5e90606e1c93ca3eaba0f273a5
Serge-boro <serhiiborodin83@gmail.com>
2023-09-08 19:43:35 -0400

    added marketing deployment

--- packages/container/config/webpack.prod.js
+++ packages/container/config/webpack.prod.js
@@ -9,7 +9,7 @@ const prodConfig = {
   mode: 'production',
   output: {
     filename: '[name].[contenthash].js',
-    publicPath: '/container/latest/'
+    publicPath: '/container/latest/',
   },
   plugins: [
     new ModuleFederationPlugin({
@@ -23,3 +23,4 @@ const prodConfig = {
 }
 
 module.exports = merge(commonConfig, prodConfig)
+
