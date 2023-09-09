19abc7ea7ef8c219bd4da4177a24131cb6e2ed5a
Serge-boro <serhiiborodin83@gmail.com>
2023-09-08 20:03:35 -0400

    first path

--- packages/container/src/components/MarketingApp.js
+++ packages/container/src/components/MarketingApp.js
@@ -1,7 +1,5 @@
 import { mount } from 'marketingCont/MarketingApp'
-import { useRef, useEffect } from 'react'
-
-import React from 'react'
+import React, { useRef, useEffect } from 'react'
 
 const MarketingApp = () => {
   const ref = useRef(null)
